bbb015329674e80254c89d5a5035c9267034a0d2
neaj09 <iwayne5@live.fr>
2023-04-02 12:20:47 +0800

    changement url

--- src/services/caller.service.js
+++ src/services/caller.service.js
@@ -4,7 +4,7 @@ import router from "@/router"
 
 // URL d'accès à mon serveur
 const Axios = axios.create({
-    baseURL: 'https://newdelice.onrender.com'
+    baseURL: 'https://back-8hi1.onrender.com'
 })
 
 // intercepte la requête et ajoute le token dans l'entête Authorization
