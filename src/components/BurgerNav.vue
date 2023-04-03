354613aaabc9e91ae15c3c9413caf024d6efd1cd
neaj09 <iwayne5@live.fr>
2023-04-03 22:05:50 +0800

    tentative de reparer la connerie

--- src/components/BurgerNav.vue
+++ src/components/BurgerNav.vue
@@ -1,33 +1,33 @@
 <template>
-  <div class="navBar"><ul class="navUl">
-        <li><router-link class="navBarLink" to="/">Home</router-link></li>
-        <li><router-link class="navBarLink" to="/menu">CARTE</router-link></li>
-        <li><router-link class="navBarLink" to="/admin/dashboard">Admin</router-link></li>
-        <li><router-link class="navBarLink" to="/">Home</router-link></li>
-        <li><router-link class="navBarLink" to="/menu">CARTE</router-link></li>
-        <li><router-link class="navBarLink" to="/admin/dashboard">Admin</router-link></li>
-      </ul>
+  <div class="navBar">
+    <ul class="navUl" v-show="!menuIsActive">
+      <li><router-link class="navBarLink" to="/">Home</router-link></li>
+      <li><router-link class="navBarLink" to="/menu">CARTE</router-link></li>
+      <li><router-link class="navBarLink" to="/admin/dashboard">Admin</router-link></li>
+      <li><router-link class="navBarLink" to="/">Home</router-link></li>
+      <li><router-link class="navBarLink" to="/menu">CARTE</router-link></li>
+      <li><router-link class="navBarLink" to="/admin/dashboard">Admin</router-link></li>
+    </ul>
   </div>
   <label>
     <input type="checkbox" v-model="menuIsActive">
     <span class="menu">
-      <!-- <span class="hamburger"></span> -->
-      <img id="logo" src="@/assets/icons/logo.svg">
+      <span class="exit" :class="{'halbaxit': menuIsActive}" ></span>
+      <img class="logo" src="@/assets/icons/logo.svg" :class="{'halba': menuIsActive}">
     </span>
   </label>
 
-    <div @click="navigation" @wheel.prevent @touchmove.prevent @scroll.prevent :class="{'showNavLink' : menuIsActive}" class="navLink">
-      <ul class="ul-link">
-        <li><router-link class="link" to="/">Home</router-link></li>
-        <li><router-link class="link" to="/menu">LA CARTE</router-link></li>
-        <li><router-link class="link" to="/admin/dashboard">Admin</router-link></li>
-        <li><router-link class="link" to="/login">Login</router-link></li>
-        <li><router-link class="link" to="/signup">Signup</router-link></li>
-        <li><router-link class="link" to="/about">About</router-link></li>
-      </ul>
-    </div>
-
-  
+  <div @click="navigation" @wheel.prevent @touchmove.prevent @scroll.prevent :class="{ 'showNavLink': menuIsActive }"
+    class="navLink">
+    <ul class="ul-link">
+      <li><router-link class="link" to="/">Home</router-link></li>
+      <li><router-link class="link" to="/menu">LA CARTE</router-link></li>
+      <li><router-link class="link" to="/admin/dashboard">Admin</router-link></li>
+      <li><router-link class="link" to="/login">Login</router-link></li>
+      <li><router-link class="link" to="/signup">Signup</router-link></li>
+      <li><router-link class="link" to="/about">About</router-link></li>
+    </ul>
+  </div>
 </template>
   
 <script>
@@ -45,7 +45,7 @@ export default {
 
 
   methods: {
-    navigation(){
+    navigation() {
       this.menuIsActive = false
     }
   }
@@ -53,7 +53,6 @@ export default {
 </script>
   
 <style scoped>
-
 ::-webkit-scrollbar {
   height: 0px;
   background-color: #00000000;
@@ -66,62 +65,75 @@ export default {
 }
 
 
-::webkit-scrollbar-corner{
+::webkit-scrollbar-corner {
   background-color: rgba(0, 0, 0, 0);
   height: 0;
 }
+
 ::-webkit-scrollbar-thumb:hover {
   background-color: rgba(0, 0, 0, 0);
   height: 0px;
 }
 
 
-#logo {
+.logo {
   width: 128px;
-    position: fixed;
-    top: 50px;
-    right: -42px;
-    translate: -50% -50%;
-    transform: rotate(16deg);
+  position: fixed;
+  top: 50px;
+  right: -42px;
+  translate: -50% -50%;
+  transform: rotate(16deg);
+  transition: 0.5s ease-in-out;
+  opacity: 1;
 }
 
 .navBar {
   z-index: 9;
-    width: 100%;
-    position: fixed;
-    top: 0px;
-    height: 110px;
-    background-image: linear-gradient(180deg, #000000 -30%, #21212100);
-    display: flex;
-    /* TEST */
-    overflow-x: scroll;
+  width: 100%;
+  position: fixed;
+  top: 0px;
+  height: 110px;
+  background-image: linear-gradient(180deg, #000000 -30%, #21212100);
+  display: flex;
+  /* TEST */
+  overflow-x: scroll;
 }
 
 .navUl {
   display: flex;
-    width: 66%;
-    font-family: 'Subtlecurves';
-    font-size: 1.3em;
-    list-style: none;
-    transition: .3s 0.1s ease-in-out;
-    flex-direction: row;
-    align-items: center;
-    padding: 0;
-    margin: 0;
-    overflow: scroll;
-    padding: 3% 3% 3% 5%;
+  width: 66%;
+  font-family: 'Subtlecurves';
+  font-size: 1.3em;
+  list-style: none;
+  transition: .3s 0.1s ease-in-out;
+  flex-direction: row;
+  align-items: center;
+  padding: 0;
+  margin: 0;
+  overflow: scroll;
+  padding: 3% 3% 3% 5%;
 
 }
 
-.navBarLink{
-  padding-right: 7px;
-  padding-left: 7px;
+
+.navUl li {
+  padding-left: 10px;
+  padding-right: 10px;
+}
+
+.navBarLink {
   color: white;
   text-decoration: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
   user-select: none;
+  /* background-color: #1c1c1c8a; */
+  padding: 5px;
+  border-radius: 6px;
+  border: 1px solid white;
 }
 
-.navUl:last-child{
+.navUl:last-child {
   padding-right: 50px;
 }
 
@@ -155,6 +167,60 @@ label .hamburger {
   border-radius: 12px;
 }
 
+label .exit{
+position: absolute;
+    top: 31px;
+    right: 35px;
+    width: 31px;
+    height: 7px;
+    background: #ffffff;
+    display: block;
+    transform-origin: center;
+    border-radius: 12px;
+    transition: 1s cubic-bezier(0, 0.19, 0.12, 1.02);
+    border-radius: 12px;
+
+    rotate: 45deg;
+    scale: (0.001);
+    
+}
+
+.halbaxit{
+    rotate: 2 13;
+    rotate: (2turn);
+    opacity: 1;
+
+    transform: rotate(3turn);
+
+}
+
+
+label .exit:after,
+label .exit:before {
+  -webkit-transition: .5s ease-in-out;
+  transition: .5s ease-in-out;
+  content: "";
+  position: absolute;
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: #ffffff;
+  border-radius: 12px;
+}
+
+label .exit:before,
+label .exit:before {
+  transform: rotate(90deg);
+    top: 0;
+}
+
+.halba{
+  transform: scale(0.0009) rotate(-4deg);
+}
+
+
+
+
 label .hamburger:after,
 label .hamburger:before {
   -webkit-transition: .5s ease-in-out;
@@ -182,15 +248,9 @@ label input {
 
 label input:checked+.menu {
   box-shadow: 0 0 0 100vw #1c1c1c00, 0 0 0 100vh #1c1c1ce0;
-  /* border-radius: 0; */
-  /* box-shadow: 0 0 0 128vw #1c1c1c, 0px 0px 0px 140vw white, 0px 0px 0 100vh #1c1c1c; */
-  /* border-radius: 0px 0px 0px 100%; */
-  /* border-bottom: 22px white solid; */
-  /* filter: blur(881px); */
+  border-radius: 100%;
   width: 0;
-  /* height: 73%; */
-  /* top: 0; */
-  /* right: 0;*/
+  height: 0;
 
 }
 
@@ -198,20 +258,21 @@ label input:checked+.menu .hamburger {
   -webkit-transform: rotate(45deg);
   transform: rotate(45deg);
 
+
 }
 
-/* label input:checked+.menu .hamburger:after {
+label input:checked+.menu .hamburger:after {
   -webkit-transform: rotate(90deg);
   transform: rotate(90deg);
   bottom: 0;
-} */
+}
 
-/* label input:checked+.menu .hamburger:before {
+label input:checked+.menu .hamburger:before {
   -webkit-transform: rotate(90deg);
   transform: rotate(90deg);
   top: 0;
 
-} */
+}
 
 
 .navLink {
@@ -225,10 +286,11 @@ label input:checked+.menu .hamburger {
   transition: .3s 0.1s ease-in-out;
 }
 
-.showNavLink{
+.showNavLink {
   visibility: visible;
   opacity: 1;
 }
+
 label input:checked+.menu+.navLink {
   opacity: 1;
 }
